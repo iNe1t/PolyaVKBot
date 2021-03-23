2c9f84c40aab0b68211cf2317cf13b44979bd7bf
iNe1t <fantiktop212@gmail.com>
2021-03-23 19:50:12 +0300

    commit

--- MemGames.py
+++ MemGames.py
@@ -4,6 +4,7 @@ from vk_api.utils import get_random_id
 
 def mafia(config.KEY, config.SERVER, config.TS):
     for event in longpoll.listen():
+        user_id = event.object.message['from_id']
         text = event.object.message['text']
         msg_id = event.object.message['id']
         username = vk.users.get(user_id=id)[0]['first_name']
